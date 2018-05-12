e286613d060030ea29cba8ade07d6c2e09d9fa5b
Ryan Rong <33141306+ryanyuerong@users.noreply.github.com>
2018-05-12 17:14:47 -0400

    Update 1_Writing a Script to Insert an Order Header.sql

--- Lab 10 Programming with Transact-SQL/1_Writing a Script to Insert an Order Header.sql
+++ Lab 10 Programming with Transact-SQL/1_Writing a Script to Insert an Order Header.sql
@@ -1,8 +1,11 @@
-DECLARE @___ datetime = GETDATE();
-DECLARE @___ datetime = DATEADD(dd, 7, GETDATE());
-DECLARE @___ int = 1;
+--Writing a Script to Insert an Order Header
+--You want to create reusable scripts that make it easy to insert sales orders. You plan to create a script to insert the order header record, and a separate script to insert order detail records for a specified order header.Both scripts will make use of variables to make them easy to reuse. Your script to insert an order header must enable users to specify values for the order date, due date, and customer ID.You are asked to include the following sales order
 
-INSERT INTO SalesLT.SalesOrderHeader (OrderDate, ___, ___, ShipMethod)
-VALUES (@OrderDate, @DueDate, @CustomerID, ___);
+DECLARE @OrderDate datetime = GETDATE();
+DECLARE @DueDate datetime = DATEADD(dd, 7, GETDATE());
+DECLARE @CustomerID int = 1;
+
+INSERT INTO SalesLT.SalesOrderHeader (OrderDate, DueDate, CustomerID, ShipMethod)
+VALUES (@OrderDate, @DueDate, @CustomerID, 'CARGO TRANSPORT 5');
 
 PRINT SCOPE_IDENTITY();
