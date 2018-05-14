f8b978ec0cdd45a57035824942250eba6d6779e7
Ryan Rong <33141306+ryanyuerong@users.noreply.github.com>
2018-05-13 20:15:51 -0400

    Update and rename Lab 10 Programming with Transact-SQL/1_Writing a Script to Insert an Order Header.sql to Lab 10 Programming with Transact-SQL.sql

---
+++ Lab 10 Programming with Transact-SQL.sql
@@ -0,0 +1,69 @@
+--Writing a Script to Insert an Order Header
+--You want to create reusable scripts that make it easy to insert sales orders. You plan to create a script to insert the order header record, and a separate script to insert order detail records for a specified order header.Both scripts will make use of variables to make them easy to reuse. Your script to insert an order header must enable users to specify values for the order date, due date, and customer ID.You are asked to include the following sales order
+
+DECLARE @OrderDate datetime = GETDATE();
+DECLARE @DueDate datetime = DATEADD(dd, 7, GETDATE());
+DECLARE @CustomerID int = 1;
+
+INSERT INTO SalesLT.SalesOrderHeader (OrderDate, DueDate, CustomerID, ShipMethod)
+VALUES (@OrderDate, @DueDate, @CustomerID, 'CARGO TRANSPORT 5');
+
+PRINT SCOPE_IDENTITY();
+
+
+
+--Extend Script to Insert an Order Detail
+--As a next step, you want to insert an order detail. The script for this must enable users to specify a sales order ID, a product ID, a quantity, and a unit price.
+--The script should check if the specified sales order ID exists in the SalesLT.SalesOrderHeader table. This can be done with the EXISTS predicate.
+--If it does, the code should insert the order details into the SalesLT.SalesOrderDetail table (using default values or NULL for unspecified columns).
+--If the sales order ID does not exist in the SalesLT.SalesOrderHeader table, the code should print the message 'The order does not exist'.
+
+-- Code from previous exercise
+DECLARE @OrderDate datetime = GETDATE();
+DECLARE @DueDate datetime = DATEADD(dd, 7, GETDATE());
+DECLARE @CustomerID int = 1;
+INSERT INTO SalesLT.SalesOrderHeader (OrderDate, DueDate, CustomerID, ShipMethod)
+VALUES (@OrderDate, @DueDate, @CustomerID, 'CARGO TRANSPORT 5');
+DECLARE @OrderID int = SCOPE_IDENTITY();
+
+-- Additional script to complete
+DECLARE @ProductID int = 760;
+DECLARE @Quantity int = 1;
+DECLARE @UnitPrice money = 782.99;
+
+IF EXISTS (SELECT * FROM SalesLT.SalesOrderHeader WHERE @OrderID = SalesOrderID)
+BEGIN
+	INSERT INTO SalesLT.SalesOrderDetail (SalesOrderID, OrderQty, ProductID, UnitPrice)
+	VALUES (@OrderID, @Quantity, @ProductID, @UnitPrice)
+END
+ELSE
+BEGIN
+	PRINT 'The order does not exist'
+END
+
+--Updating Bike Prices
+--Adventure Works has determined that the market average price for a bike is $2,000, and consumer research has indicated that the maximum price any customer would be likely to pay for a bike is $5,000.
+--You must write some Transact-SQL logic that incrementally increases the list price for all bike products by 10% until the average list price for a bike is at least the same as the market average, or until the most expensive bike is priced above the acceptable maximum indicated by the consumer research.
+--The product categories in the Bikes parent category can be determined from the SalesLT.vGetAllCategories view.
+
+DECLARE @MarketAverage money = 2000;
+DECLARE @MarketMax money = 5000;
+DECLARE @AWMax money;
+DECLARE @AWAverage money;
+
+SELECT @AWAverage = AVG(ListPrice), @AWMax = MAX(ListPrice)
+FROM SalesLT.Product
+WHERE ProductCategoryID IN
+	(SELECT DISTINCT ProductCategoryID
+	 FROM SalesLT.vGetAllCategories
+	 WHERE ParentProductCategoryName = 'Bikes');
+
+WHILE @AWAverage < @MarketAverage
+BEGIN
+   UPDATE SalesLT.Product
+   SET ListPrice = ListPrice * 1.1
+   WHERE ProductCategoryID IN
+	(SELECT DISTINCT ProductCategoryID
+	 FROM SalesLT.vGetAllCategories
+	 WHERE ParentProductCategoryName = 'Bikes');
+
